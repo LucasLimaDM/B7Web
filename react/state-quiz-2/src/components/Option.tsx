6bcb2fdb6f2df073c591c7761ffc5e4201a9e62c
LucasLDias <lucaslim.dias@gmail.com>
2024-04-30 10:00:14 -0300

    add: marcação única de resposta

--- react/state-quiz-2/src/components/Option.tsx
+++ react/state-quiz-2/src/components/Option.tsx
@@ -18,34 +18,43 @@ export function Option({text, optNum, activeQuestionId, setAnswers, answers}: Pr
 
   function optionClick(){
     //optNum -1 pois a option 0 representa nenhuma option, enquanto nas respostas 0 já é a primeira opção
-    const isAnswerRight = questApi.validateAnswer(activeQuestionId, (optNum - 1));
 
-    const stateNum = isAnswerRight ? 2 : 1
+    if(answers[activeQuestionId].option === 0){
 
-    const newAnswers = [...answers]
-    newAnswers[activeQuestionId].option = optNum;
-    newAnswers[activeQuestionId].state = stateNum
-    setAnswers(newAnswers)
+      const isAnswerRight = questApi.validateAnswer(activeQuestionId, (optNum - 1));
+
+      const stateNum = isAnswerRight ? 2 : 1
+
+      const newAnswers = [...answers]
+      newAnswers[activeQuestionId].option = optNum;
+      newAnswers[activeQuestionId].state = stateNum
+      setAnswers(newAnswers)
+
+    }
+
+    
   }
 
   function getOptionStyles(){
     
     if(activeAnswer.option === optNum){
       if(activeAnswer.state === 2){
-        return ' bg-green-300 hover:bg-green-400 '
+        return ' bg-green-300 cursor-default'
       } else if(activeAnswer.state === 1){
-        return ' bg-red-300 hover:bg-red-400 '
+        return ' bg-red-300 cursor-default'
       } else {
         return ''
       }
+    } else if(activeAnswer.option !== 0) {
+      return ''
     } else {
-      return 'hover:bg-slate-300'
+      return 'hover:bg-slate-300 cursor-pointer'
     }
   }
 
   return (
     <div
-      className={getOptionStyles() + ' w-40 p-3 m-4 border cursor-pointer '}
+      className={getOptionStyles() + ' w-40 p-3 m-4 border '}
       onClick={optionClick}
     >
 
