6da7561a66082491de8c477dd48255f62ffec0b9
LucasLDias <lucaslim.dias@gmail.com>
2024-05-13 14:56:40 -0300

    início novo projeto + bug fix git

---
+++ react/04-neuroimage/neuroimage-viewer/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { PatientAdd } from "@/components/menus/PatientAdd";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <PatientAdd />
+    
+    </main>
+  );
+}
---
+++ react/04-neuroimage/neuroimage-viewer/src/components/Header.tsx
@@ -0,0 +1,6 @@
+import { useContext } from "react"
+
+
+export const Header = ()=>{
+  const pgCtx = useContext(pageContext);
+}
